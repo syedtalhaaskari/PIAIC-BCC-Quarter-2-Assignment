c93d969f4bbe437c17f62e4790e1930328125288
syedtalhaaskari <88083531+syedtalhaaskari97@users.noreply.github.com>
2023-02-08 10:48:04 +0500

    Step 01 Completed

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var message = "Hello World";
-message = 6;
-console.log(message);
+//strongly typed syntax
+var a = "Pakistan";
+a = "USA";
+var b = 9;
+var c = true;
+//type inference
+var e = "USA";
+var f = 10.9;
+f = 22;
+var g = false;
+g = true;
--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
-let message = "Hello World";
+//strongly typed syntax
+let a: string = "Pakistan";
+a = "USA";
+let b: number = 9;
+let c: boolean = true;
 
-message = 6;
-
-console.log(message);
+//type inference
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
