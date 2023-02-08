f31f1e85418e5358a1b4618f915fc739cd53bcc3
syedtalhaaskari <88083531+syedtalhaaskari97@users.noreply.github.com>
2023-02-08 10:54:53 +0500

    Step 02 Completed

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-//strongly typed syntax
-var a = "Pakistan";
-a = "USA";
-var b = 9;
-var c = true;
-//type inference
-var e = "USA";
-var f = 10.9;
-f = 22;
-var g = false;
-g = true;
+//use const where variable values do not change
+var a = 5;
+var b = 33;
+var c = "best";
+//I suggest use let instead of var everywhere, 
+//becuase let has blocked scope
+if (true) {
+    var z = 4;
+    //use z
+}
+else {
+    var z = "string";
+    //use z
+}
+console.log("let: " + z); // Error: z is not defined in this scope
